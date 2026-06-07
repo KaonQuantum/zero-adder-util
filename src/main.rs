ed1687187c5c65a83b020ed302c31a42e71c3ae4
VidurThammineni <vidur.thammineni@gmail.com>
2026-06-07 13:14:50 -0700

    Version 0.1.1! More verbose and added an option for better digit adding

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs, path::{Path, PathBuf}};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use clap::Parser;
 
@@ -19,6 +22,11 @@ struct Cli {
     /// Decrease the amount of leading zeros rather than raising it
     #[arg(short, long)]
     decrease: bool,
+
+    /// Add zeros like problem-1 & problem-11 -> problem-01 & problem-011 instead of -> problem-01 &
+    /// problem-11
+    #[arg(short, long)]
+    basic_add: bool,
 }
 
 fn main() {
@@ -27,7 +35,18 @@ fn main() {
 
     if let Ok(does_exist) = fs::exists(&path) {
         if does_exist {
-            change_leading_zeros(&path, args.decrease, args.number);
+            let num =
+                change_leading_zeros(&path, args.decrease, args.number as usize, args.basic_add);
+            if num != 0 {
+                println!(
+                    "{}{}{}",
+                    "Successfully changed ".green(),
+                    num.green(),
+                    " filenames.".green()
+                );
+            } else {
+                println!("{}", "Changed zero filenames.".red());
+            }
         } else {
             println!("{}", "Path does not exist".red());
         }
@@ -36,11 +55,45 @@ fn main() {
     }
 }
 
-fn change_leading_zeros(path: &Path, dec: bool, count: u8) {
+fn change_leading_zeros(path: &Path, dec: bool, count: usize, basic: bool) -> usize {
     let filenames = get_filenames(path);
 
+    if basic && dec {
+        println!(
+            "{}",
+            "Basic is only an option for increasing. Decreasing normally.".yellow()
+        );
+    }
+
+    let mut target: usize = 0;
+
+    if !basic && !dec {
+        let mut minimum = usize::MAX;
+        for file in &filenames {
+            if !file.chars().last().unwrap_or(' ').is_ascii_digit() {
+                continue;
+            }
+
+            let mut split_index = 0;
+            for (i, j) in file.char_indices().rev() {
+                if !j.is_ascii_digit() {
+                    split_index = i + j.len_utf8();
+                    break;
+                }
+            }
+
+            minimum = minimum.min(file[split_index..].len())
+        }
+
+        target = minimum + count;
+    }
+
+    let mut files_changed: usize = 0;
+
     for file in filenames {
-        if !file.chars().last().unwrap_or(' ').is_ascii_digit() { continue; }
+        if !file.chars().last().unwrap_or(' ').is_ascii_digit() {
+            continue;
+        }
 
         let mut split_index = 0;
         for (i, j) in file.char_indices().rev() {
@@ -52,10 +105,28 @@ fn change_leading_zeros(path: &Path, dec: bool, count: u8) {
 
         let prefix = &file[..split_index];
         let numeric = &file[split_index..];
-        
+
         if !dec {
-            let new = format!("{}{}{}", prefix, "0".repeat(count as usize), numeric);
-            let _ = fs::rename(path.join(&file), path.join(new));
+            let new = if basic {
+                format!("{}{}{}", prefix, "0".repeat(count), numeric)
+            } else {
+                let pad = target.saturating_sub(numeric.len());
+                format!("{}{}{}", prefix, "0".repeat(pad), numeric)
+            };
+
+            let success = fs::rename(path.join(&file), path.join(&new));
+
+            if let Err(fail) = success {
+                println!(
+                    "File rename {} -> {} failed with: {}",
+                    &file.blue(),
+                    &new.blue(),
+                    fail.red()
+                );
+            } else {
+                files_changed += 1;
+            }
+
             continue;
         }
 
@@ -63,21 +134,35 @@ fn change_leading_zeros(path: &Path, dec: bool, count: u8) {
             .chars()
             .take_while(|&c| c == '0')
             .count()
-            .min(count as usize)
+            .min(count)
             .min(numeric.len() - 1);
         let new = format!("{}{}", prefix, &numeric[remove..]);
-        let _ = fs::rename(path.join(&file), path.join(new));
+        let success = fs::rename(path.join(&file), path.join(&new));
+
+        if let Err(fail) = success {
+            println!(
+                "File rename {} -> {} failed with: {}",
+                &file.blue(),
+                &new.blue(),
+                fail.red()
+            );
+        } else {
+            files_changed += 1;
+        }
     }
+
+    files_changed
 }
 
 fn get_filenames(path: &Path) -> Vec<String> {
     let mut names = Vec::new();
     if let Ok(read_dir) = fs::read_dir(path) {
         for file in read_dir.flatten() {
-                names.push(file
-                    .file_name()
+            names.push(
+                file.file_name()
                     .into_string()
-                    .unwrap_or("unknown name".into()));
+                    .unwrap_or("unknown name".into()),
+            );
         }
     }
     names
